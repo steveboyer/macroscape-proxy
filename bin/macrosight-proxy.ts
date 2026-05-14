8410ec1b67680438fa4fd46bbcab08ec03ec5a10
Steve Boyer <steveboyer@users.noreply.github.com>
2026-05-13 20:57:31 -0600

    Add GitHub OIDC stack and CI/CD workflows

--- bin/macrosight-proxy.ts
+++ bin/macrosight-proxy.ts
@@ -1,20 +1,20 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib/core';
 import { MacrosightProxyStack } from '../lib/macrosight-proxy-stack';
+import { GithubOidcStack } from '../lib/github-oidc-stack';
 
 const app = new cdk.App();
-new MacrosightProxyStack(app, 'MacrosightProxyStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const env = {
+  account: process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEFAULT_REGION,
+};
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+new MacrosightProxyStack(app, 'MacrosightProxyStack', { env });
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+new GithubOidcStack(app, 'MacrosightProxyGithubOidcStack', {
+  env,
+  githubOwner: 'steveboyer',
+  githubRepo: 'macrosight-proxy',
+  allowedBranches: ['main'],
 });
---
+++ lib/github-oidc-stack.ts
@@ -0,0 +1,70 @@
+import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import * as iam from 'aws-cdk-lib/aws-iam';
+
+export interface GithubOidcStackProps extends cdk.StackProps {
+  githubOwner: string;
+  githubRepo: string;
+  allowedBranches?: string[];
+}
+
+export class GithubOidcStack extends cdk.Stack {
+  public readonly deployRoleArn: string;
+
+  constructor(scope: Construct, id: string, props: GithubOidcStackProps) {
+    super(scope, id, props);
+
+    const { githubOwner, githubRepo, allowedBranches = ['main'] } = props;
+
+    const githubProvider = new iam.OpenIdConnectProvider(this, 'GithubProvider', {
+      url: 'https://token.actions.githubusercontent.com',
+      clientIds: ['sts.amazonaws.com'],
+    });
+
+    const subjectClaims = allowedBranches.map(
+      (branch) => `repo:${githubOwner}/${githubRepo}:ref:refs/heads/${branch}`,
+    );
+
+    const deployRole = new iam.Role(this, 'GithubDeployRole', {
+      roleName: 'MacrosightProxyGithubDeployRole',
+      assumedBy: new iam.FederatedPrincipal(
+        githubProvider.openIdConnectProviderArn,
+        {
+          StringEquals: {
+            'token.actions.githubusercontent.com:aud': 'sts.amazonaws.com',
+          },
+          'ForAnyValue:StringLike': {
+            'token.actions.githubusercontent.com:sub': subjectClaims,
+          },
+        },
+        'sts:AssumeRoleWithWebIdentity',
+      ),
+      maxSessionDuration: cdk.Duration.hours(1),
+    });
+
+    // Allow this role to assume the CDK bootstrap roles, which is what
+    // `cdk deploy` actually uses to do its work.
+    const account = cdk.Stack.of(this).account;
+    const region = cdk.Stack.of(this).region;
+    const bootstrapQualifier = 'hnb659fds';
+
+    deployRole.addToPolicy(
+      new iam.PolicyStatement({
+        actions: ['sts:AssumeRole'],
+        resources: [
+          `arn:aws:iam::${account}:role/cdk-${bootstrapQualifier}-deploy-role-${account}-${region}`,
+          `arn:aws:iam::${account}:role/cdk-${bootstrapQualifier}-file-publishing-role-${account}-${region}`,
+          `arn:aws:iam::${account}:role/cdk-${bootstrapQualifier}-image-publishing-role-${account}-${region}`,
+          `arn:aws:iam::${account}:role/cdk-${bootstrapQualifier}-lookup-role-${account}-${region}`,
+        ],
+      }),
+    );
+
+    this.deployRoleArn = deployRole.roleArn;
+
+    new cdk.CfnOutput(this, 'DeployRoleArn', {
+      value: deployRole.roleArn,
+      description: 'IAM role ARN for GitHub Actions to assume',
+    });
+  }
+}
